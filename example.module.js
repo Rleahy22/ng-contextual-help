85f55f23c24cc419f151e9bde15f1d6ce4ff6441
Ryan Leahy <ryanleahy90@gmail.com>
2016-10-04 23:18:35 -0400

    added image

--- example.module.js
+++ example.module.js
@@ -9,7 +9,8 @@ class exampleCtrl {
             firstNav: 'Takes you to page 1 (but not really in this dummy page)',
             secondNav: 'Takes you to page 2 (If this page were real)',
             thirdNav: 'Takes you to page 3 (Seriously these links are fake)',
-            checkbox: 'This checkbox allows you to pick things'
+            checkbox: 'This checkbox allows you to pick things',
+            image: 'Yeah, this is a bear'
         };
     }
 }
