cc615a134033a1e75470cbd256683df2d0c9e38e
Ryan Leahy <ryanleahy90@gmail.com>
2016-10-04 20:12:19 -0400

    added content to exampl

--- example.module.js
+++ example.module.js
@@ -6,7 +6,10 @@ var exampleApp = angular.module('exampleApp', [
 class exampleCtrl {
     constructor () {
         this.textPairs = {
-            intro: 'This is the intro'
+            firstNav: 'Takes you to page 1 (but not really in this dummy page)',
+            secondNav: 'Takes you to page 2 (If this page were real)',
+            thirdNav: 'Takes you to page 3 (Seriously these links are fake)',
+            checkbox: 'This checkbox allows you to pick things'
         };
     }
 }
